e0274eb7696b08ccb8801baaecf220c9d542898d
DavidCuy <david.cuy.sanchez@gmail.com>
2023-09-02 16:30:04 -0600

    feat: migrate mqtt manager logic

--- src/domain/services/mqtt-manager.service.ts
+++ src/domain/services/mqtt-manager.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject, map } from 'rxjs';
 import { MqttService, IMqttMessage, IOnConnectEvent, IOnErrorEvent } from 'ngx-mqtt';
+import { TopicMessage } from 'src/data/repositories/mqtt/entities/topic-entity';
+import { MqttMessage } from 'src/data/repositories/mqtt/entities/message-entity';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class MqttManagerService {
+  private currentTopicSubject: BehaviorSubject<TopicMessage> = new BehaviorSubject({} as TopicMessage);
+  public readonly currentTopic: Observable<TopicMessage> = this.currentTopicSubject.asObservable();
+  private prevMillis: number = new Date().getTime();
+  public all_topic_messages: Array<TopicMessage> = [];
+  public all_subscribed_topics: Array<string> = [];
+  public selected_topic_str: string = '';
+
   constructor(private _mqttService: MqttService) {
     this._mqttService.onConnect.subscribe(async (connectStatus: IOnConnectEvent) => {
       console.log('CONNECT MQTT');
@@ -75,5 +84,57 @@ export class MqttManagerService {
 
   private _onConnectMqtt(): void {
 
+  }
+
+  public add_topic_to_subscription(input_topic2sub: string): void {
+    const selected_tm: TopicMessage = {
+      topic: input_topic2sub,
+      messages: [],
+      subscription: this.subscribe(input_topic2sub).subscribe((message: IMqttMessage) => {
+        console.log(message)
+        if (new Date().getTime() - this.prevMillis < 100) return
+        this.prevMillis = new Date().getTime()
+        const wildcard_topics = this.all_subscribed_topics.filter(m => this.wildcard(message.topic, m) !== null )
+        if (wildcard_topics.length > 0) {
+          let payload = "";
+          try {
+            payload = JSON.parse(message.payload.toString());
+          } catch (e) {
+            payload = message.payload.toString();
+          }
+
+          wildcard_topics.forEach(t => {
+            let selected_tm = this.all_topic_messages.find((tm) => tm.topic == t);
+            if(selected_tm === null || selected_tm === undefined) return
+            const mqtt_message: MqttMessage = {
+              datetime: new Date(),
+              payload: payload,
+              topic: t
+            }
+            selected_tm?.messages.unshift(mqtt_message)
+          });
+        }
+      })
+    }
+    this.all_topic_messages.push(selected_tm);
+    this.all_subscribed_topics.push(input_topic2sub);
+    this.change_current_topic(input_topic2sub)
+  }
+
+  public remove_topic_subscription(input_topic: string): void {
+    const sub_topic = this.all_topic_messages.find((tm) => tm.topic === input_topic)
+    if(sub_topic === null || sub_topic === undefined) return
+    sub_topic.subscription.unsubscribe();
+
+    this.all_topic_messages = this.all_topic_messages.filter((tm) => tm.topic !== input_topic);
+    this.all_subscribed_topics = this.all_subscribed_topics.filter((t) => t !== input_topic);
+  }
+
+  public change_current_topic(input_topic: string): void {
+    const evaluated_tm = this.all_topic_messages.find((tm) => tm.topic === input_topic);
+    if(evaluated_tm === null || evaluated_tm === undefined) return
+
+    this.currentTopicSubject.next(evaluated_tm)
+    this.selected_topic_str = input_topic
   }
 }
--- src/presentation/app/mqtt-client/mqtt-test/mqtt-test.component.ts
+++ src/presentation/app/mqtt-client/mqtt-test/mqtt-test.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { IMqttMessage } from 'ngx-mqtt';
 import { MqttManagerService } from '../../../../domain/services/mqtt-manager.service';
-import { TopicMessage } from 'src/data/repositories/mqtt/entities/topic-entity';
 import { MqttMessage } from 'src/data/repositories/mqtt/entities/message-entity';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-mqtt-test',
@@ -11,24 +10,21 @@ import { MqttMessage } from 'src/data/repositories/mqtt/entities/message-entity'
 })
 export class MqttTestComponent implements OnInit, OnDestroy {
   select_tabname = 'subscribe'
-  selected_topic: string = '';
 
-  subscribed_topics: Array<string> = [];
-  topic_messages: Array<TopicMessage> = [];
   displayed_messages: Array<MqttMessage> = [];
 
-  prevMillis: number = new Date().getTime()
+  prevMillis: number = new Date().getTime();
 
-  constructor(private mqttManager: MqttManagerService) {
+  current_topic_sub: Subscription;
+
+  constructor(public mqttManager: MqttManagerService) {
   }
 
-  ngOnInit(): void {
-    this.topic_messages = []
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
-      this.topic_messages.forEach((tm) => {
-        tm.subscription.unsubscribe()
+      this.mqttManager.all_topic_messages.forEach((tm) => {
+        this.mqttManager.remove_topic_subscription(tm.topic)
       })
   }
 
@@ -37,72 +33,38 @@ export class MqttTestComponent implements OnInit, OnDestroy {
   }
 
   add_topic_subscribe(input_topic2sub: string): void {
-    console.log(`Topic to subscribe ${input_topic2sub}`)
-    if (this.subscribed_topics.includes(input_topic2sub)) return
+    if (this.mqttManager.all_subscribed_topics.includes(input_topic2sub)) return
+    this.mqttManager.add_topic_to_subscription(input_topic2sub)
 
-    this.subscribed_topics.unshift(input_topic2sub);
-    this.selected_topic = input_topic2sub
 
-    const selected_tm: TopicMessage = {
-      topic: this.selected_topic,
-      messages: [],
-      subscription: this.mqttManager.subscribe(input_topic2sub).subscribe((message: IMqttMessage) => {
-        if (new Date().getTime() - this.prevMillis < 100) return
-        this.prevMillis = new Date().getTime()
-        const wildcard_topics = this.subscribed_topics.filter(m => this.mqttManager.wildcard(message.topic, m) !== null )
-        if (wildcard_topics.length > 0) {
-          let payload = "";
-          try {
-            payload = JSON.parse(message.payload.toString());
-          } catch (e) {
-            payload = message.payload.toString();
-          }
+    this.mqttManager.currentTopic.subscribe(topic => {
+      console.log(this.mqttManager.all_subscribed_topics)
+    })
 
-          wildcard_topics.forEach(t => {
-            let selected_tm = this.topic_messages.find((tm) => tm.topic == t);
-            console.log(selected_tm)
-            if(selected_tm === null || selected_tm === undefined) return
-            const mqtt_message: MqttMessage = {
-              datetime: new Date(),
-              payload: payload,
-              topic: t
-            }
-            selected_tm?.messages.unshift(mqtt_message)
-            console.log(this.topic_messages)
-          });
-        }
-      })
-    }
-    this.topic_messages.push(selected_tm)
-
-    const search_tm = this.topic_messages.find((tm) => tm.topic == this.selected_topic);
-    if(search_tm !== null && search_tm !== undefined) {
-      this.displayed_messages = search_tm.messages
-    }
+    this.mqttManager.currentTopic.subscribe((topic) => {
+      this.displayed_messages = topic.messages
+    })
   }
 
   select_topic(topic: string) {
-    console.log(topic)
-    this.selected_topic = topic;
+    this.mqttManager.change_current_topic(topic);
 
-    const selected_tm = this.topic_messages.find((tm) => tm.topic === topic);
-    if(selected_tm === null || selected_tm === undefined) return
-    this.displayed_messages = selected_tm.messages
+    this.mqttManager.currentTopic.subscribe((topic) => {
+      console.log(topic)
+      this.displayed_messages = topic.messages
+    })
   }
 
   remove_topic(topic: string) {
     console.log(`Remove topic => ${topic}`);
-    const sub_topic = this.topic_messages.find((tm) => tm.topic === topic)
-    console.log(sub_topic)
-    if(sub_topic === null || sub_topic === undefined) return
-    sub_topic.subscription.unsubscribe();
-
-    this.topic_messages = this.topic_messages.filter((tm) => tm.topic !== topic);
-    this.subscribed_topics = this.subscribed_topics.filter((t) => t !== topic);
-    console.log(this.topic_messages)
+    this.mqttManager.remove_topic_subscription(topic);
   }
 
   publish_to_topic(topic2publish: string, text2publish: string) {
+    if (topic2publish == '') {
+      alert('El campo del tema no puede ser vacío')
+      return
+    }
     this.mqttManager.publish(topic2publish, text2publish);
 
   }
