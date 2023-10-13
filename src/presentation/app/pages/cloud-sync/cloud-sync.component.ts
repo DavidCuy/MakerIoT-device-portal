dfc80cef96d13e5404d4c55651d79141be0dac98
DavidCuy <david.cuy.sanchez@gmail.com>
2023-10-12 23:50:30 -0600

    feat: first use of cloud sync view

--- src/presentation/app/pages/cloud-sync/cloud-sync.component.ts
+++ src/presentation/app/pages/cloud-sync/cloud-sync.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { CloudProviderIndexUseCase } from 'src/domain/usecases/cloudProvider/index.usecase';
+import { CloudConfigIndexUseCase } from 'src/domain/usecases/cloudConfig/index.usecase';
+import { CloudProviderModel } from 'src/domain/models/cloudProvider.model';
+import { CloudConfigModel } from 'src/domain/models/cloudConfig.model';
+import { IndexEntity } from 'src/data/repositories/index/entitites/index-entity';
 
 @Component({
   selector: 'app-cloud-sync',
@@ -7,13 +12,32 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./cloud-sync.component.css']
 })
 export class CloudSyncComponent implements OnInit {
+  PAGE_SIZE: number = 10;
 
-  constructor(private toastr: ToastrService) {
+  indexCloudConfigObject: IndexEntity<CloudConfigModel>;
+  cloudProviders: CloudProviderModel[] = []
+  cloudConfigs: CloudConfigModel[] = []
+
+  currentPage: number = 1;
+
+  constructor(
+    private cloudProviderIndexUsecase: CloudProviderIndexUseCase,
+    private cloudConfigIndexUseCase: CloudConfigIndexUseCase,
+    private toastr: ToastrService) {
     
   }
 
   ngOnInit(): void {
-    
+    this.cloudProviderIndexUsecase.execute().subscribe((resp) => {
+      this.cloudProviders = resp.Data;
+      console.log(this.cloudProviders)
+    })
+
+    this.cloudConfigIndexUseCase.execute({ relationships: ['cloudProvider'], perPage: this.PAGE_SIZE, page: this.currentPage }).subscribe((resp) => {
+      this.indexCloudConfigObject = resp;
+      this.cloudConfigs = this.indexCloudConfigObject.Data;
+      console.log(this.cloudConfigs)
+    })
   }
 
 }
